beeb298e07f4bc00a7059a0dae7862e70a06e83f
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2024-03-01 19:00:32 +0600

    layout updated

--- resources/views/admin/partials/dashboard.blade.php
+++ resources/views/admin/partials/dashboard.blade.php
@@ -3,111 +3,120 @@
 
     <main>
 
-        <div class="py-2 px-10 sm:ml-64">
-            <div class="py-8 rounded-lg flex justify-between">
-                <h2 class="text-2xl">Welcome, <span
-                        class="text-blue-700">{{ $adminData->name }}</span>.</h2>
-                <h2 class="text-sm text-gray-500 text-right">
-                    {{ now('Asia/Dhaka')->format('M d, Y - h:i A') }}
-                </h2>
-            </div>
-
-            <div class="px-40 py-8 flex flex-wrap gap-10 justify-center">
-
-                <div class="max-w-sm w-72 p-4 bg-white border border-gray-200 rounded-lg shadow">
-                    <div class="p-2 flex justify-between items-center">
-                        <h5 class=" text-xl font-bold tracking-tight text-gray-900">Total
-                            Admins</h5>
-                        <img src="{{ asset('asset/img/dashboard/admin.gif') }}" alt=""
-                             class="size-24">
-                    </div>
-                    <div class="p-2 py-5 flex items-center justify-center">
-                        <p
-                            class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
-                           font-medium text-center text-white bg-teal-400 rounded-lg">
-                            {{ $admin }}
-                        </p>
-                    </div>
+        <div class="p-2 sm:ml-64">
+            <div class="bg-gray-200 m-8 rounded-lg p-4 px-4">
+                <div class="py-8 rounded-lg flex justify-between">
+                    <h2 class="text-2xl">Welcome, <span
+                            class="text-blue-700">{{ $adminData->name }}</span>.</h2>
+                    <h2 class="text-sm text-gray-500 text-right">
+                        {{ now('Asia/Dhaka')->format('M d, Y - h:i A') }}
+                    </h2>
                 </div>
 
-                <div class="max-w-sm w-72 p-4 bg-white border border-gray-200 rounded-lg shadow">
-                    <div class="p-2 flex justify-between items-center">
-                        <h5 class=" text-xl font-bold tracking-tight text-gray-900">Total
-                            Students</h5>
-                        <img src="{{ asset('asset/img/dashboard/students.gif') }}" alt=""
-                             class="size-24">
-                    </div>
-                    <div class="p-2 py-5 flex items-center justify-center">
-                        <p
-                            class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
-                           font-medium text-center text-white bg-teal-400 rounded-lg">
-                            {{ $user }}
-                        </p>
-                    </div>
-                </div>
+                <div class="pt-1 pb-5 flex flex-wrap gap-14 justify-between">
 
-                <div class="max-w-sm w-72 p-4 bg-white border border-gray-200 rounded-lg shadow">
-                    <div class="p-2 flex justify-between items-center">
-                        <h5 class=" text-xl font-bold tracking-tight text-gray-900">Categories</h5>
-                        <img src="{{ asset('asset/img/dashboard/category.gif') }}" alt=""
-                             class="size-24">
-                    </div>
-                    <div class="p-2 py-5 flex items-center justify-center">
-                        <p
-                            class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
+                    <div
+                        class="max-w-sm w-96 p-4 bg-white border border-gray-200 rounded-lg shadow">
+                        <div class="p-2 flex justify-between items-center">
+                            <h5 class=" text-xl font-bold tracking-tight text-gray-900">Total
+                                Admins</h5>
+                            <img src="{{ asset('asset/img/dashboard/admin.gif') }}" alt=""
+                                 class="size-24">
+                        </div>
+                        <div class="p-2 py-5 flex items-center justify-center">
+                            <p
+                                class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
                            font-medium text-center text-white bg-teal-400 rounded-lg">
-                            {{ $category }}
-                        </p>
+                                {{ $admin }}
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="max-w-sm w-72 p-4 bg-white border border-gray-200 rounded-lg shadow">
-                    <div class="p-2 flex justify-between items-center">
-                        <h5 class=" text-xl font-bold tracking-tight text-gray-900">Quizzes</h5>
-                        <img src="{{ asset('asset/img/dashboard/quiz.gif') }}" alt=""
-                             class="size-24">
-                    </div>
-                    <div class="p-2 py-5 flex items-center justify-center">
-                        <p
-                            class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
+                    <div
+                        class="max-w-sm w-96 p-4 bg-white border border-gray-200 rounded-lg shadow">
+                        <div class="p-2 flex justify-between items-center">
+                            <h5 class=" text-xl font-bold tracking-tight text-gray-900">Total
+                                Students</h5>
+                            <img src="{{ asset('asset/img/dashboard/students.gif') }}" alt=""
+                                 class="size-24">
+                        </div>
+                        <div class="p-2 py-5 flex items-center justify-center">
+                            <p
+                                class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
                            font-medium text-center text-white bg-teal-400 rounded-lg">
-                            {{ $quiz }}
-                        </p>
+                                {{ $user }}
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="max-w-sm w-72 p-4 bg-white border border-gray-200 rounded-lg shadow">
-                    <div class="p-2 flex justify-between items-center">
-                        <h5 class=" text-xl font-bold tracking-tight text-gray-900">Total
-                            Questions</h5>
-                        <img src="{{ asset('asset/img/dashboard/question.gif') }}" alt=""
-                             class="size-24">
-                    </div>
-                    <div class="p-2 py-5 flex items-center justify-center">
-                        <p
-                            class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
+                    <div
+                        class="max-w-sm w-96 p-4 bg-white border border-gray-200 rounded-lg shadow">
+                        <div class="p-2 flex justify-between items-center">
+                            <h5 class=" text-xl font-bold tracking-tight text-gray-900">
+                                Categories</h5>
+                            <img src="{{ asset('asset/img/dashboard/category.gif') }}" alt=""
+                                 class="size-24">
+                        </div>
+                        <div class="p-2 py-5 flex items-center justify-center">
+                            <p
+                                class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
                            font-medium text-center text-white bg-teal-400 rounded-lg">
-                            2
-                        </p>
+                                {{ $category }}
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="max-w-sm w-72 p-4 bg-white border border-gray-200 rounded-lg shadow">
-                    <div class="p-2 flex justify-between items-center">
-                        <h5 class=" text-xl font-bold tracking-tight text-gray-900">Unique
-                            Visitors</h5>
-                        <img src="{{ asset('asset/img/dashboard/visitors.gif') }}" alt=""
-                             class="size-24">
-                    </div>
-                    <div class="p-2 py-5 flex items-center justify-center">
-                        <p
-                            class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
+                    <div
+                        class="max-w-sm w-96 p-4 bg-white border border-gray-200 rounded-lg shadow">
+                        <div class="p-2 flex justify-between items-center">
+                            <h5 class=" text-xl font-bold tracking-tight text-gray-900">Quizzes</h5>
+                            <img src="{{ asset('asset/img/dashboard/quiz.gif') }}" alt=""
+                                 class="size-24">
+                        </div>
+                        <div class="p-2 py-5 flex items-center justify-center">
+                            <p
+                                class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
                            font-medium text-center text-white bg-teal-400 rounded-lg">
-                            20
-                        </p>
+                                {{ $quiz }}
+                            </p>
+                        </div>
                     </div>
-                </div>
 
+                    <div
+                        class="max-w-sm w-96 p-4 bg-white border border-gray-200 rounded-lg shadow">
+                        <div class="p-2 flex justify-between items-center">
+                            <h5 class=" text-xl font-bold tracking-tight text-gray-900">Total
+                                Questions</h5>
+                            <img src="{{ asset('asset/img/dashboard/question.gif') }}" alt=""
+                                 class="size-24">
+                        </div>
+                        <div class="p-2 py-5 flex items-center justify-center">
+                            <p
+                                class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
+                           font-medium text-center text-white bg-teal-400 rounded-lg">
+                                2
+                            </p>
+                        </div>
+                    </div>
+
+                    <div
+                        class="max-w-sm w-96 p-4 bg-white border border-gray-200 rounded-lg shadow">
+                        <div class="p-2 flex justify-between items-center">
+                            <h5 class=" text-xl font-bold tracking-tight text-gray-900">Unique
+                                Visitors</h5>
+                            <img src="{{ asset('asset/img/dashboard/visitors.gif') }}" alt=""
+                                 class="size-24">
+                        </div>
+                        <div class="p-2 py-5 flex items-center justify-center">
+                            <p
+                                class="inline-flex items-center justify-center px-3 py-2 w-full text-lg
+                           font-medium text-center text-white bg-teal-400 rounded-lg">
+                                20
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
 
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,10 +1,29 @@
 <x-app-layout>
 
     @include('components.common.sidebar')
-    <div class="py-2 px-10 sm:ml-64">
-        <div class="p-8 mt-10 rounded-lg">
-            <div class="">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+    <div class="p-2 sm:ml-64">
+        <div class="bg-gray-200 m-8 rounded-lg">
+            <div class="px-1 pt-4 rounded-lg flex items-center justify-between">
+                <ol class="flex items-center w-full p-3 space-x-2 text-sm font-medium
+                    text-center text-gray-500 border border-gray-200 rounded-lg sm:text-base sm:p-4 sm:space-x-4 rtl:space-x-reverse">
+                    <li class="flex items-center">
+                        Profile
+                        <svg class="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180" aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
+                            <path stroke="currentColor" stroke-linecap="round"
+                                  stroke-linejoin="round" stroke-width="2"
+                                  d="m7 9 4-4-4-4M1 9l4-4-4-4"/>
+                        </svg>
+                    </li>
+                    <li class="flex items-center text-blue-600">
+                        Edit Profile
+                    </li>
+                </ol>
+            </div>
+
+            <div class="pb-6 pt-2">
+                <div class="px-5 mx-auto space-y-6">
                     <div class="p-4 sm:p-8 bg-white shadow-sm sm:rounded-lg">
                         <div class="max-w-xl mx-auto">
                             @include('profile.partials.update-profile-information-form')
@@ -26,4 +45,5 @@
             </div>
         </div>
     </div>
+
 </x-app-layout>
