7279c1c6ac7409fd18b7c86627b51e45449352e3
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2024-03-20 22:07:25 +0600

    small UI updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,52 +47,70 @@
     {{-- Cards --}}
     <div class="bg-slate-100">
         <section class="mx-auto max-w-screen-xl h-96 flex justify-center items-center gap-14">
-            <div
-                class="flex flex-col gap-4 items-center max-w-sm p-6 bg-white border h-72
-                border-gray-200 rounded-lg shadow-lg">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-center text-gray-900">
-                    What's Your Leadership Style?</h5>
-                <p class="mb-3 font-normal text-gray-700 text-center pt-2 h-24">Discover the leader
-                    within and
-                    steer your path to success. Perfect for professionals looking to understand
-                    their unique leadership qualities.</p>
-                <a href="#"
-                   class="flex justify-center px-8 items-center py-2 text-sm font-medium text-center
-                   text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                    Read more
-                </a>
-            </div>
 
-            <div
-                class="flex flex-col gap-4 items-center max-w-sm p-6 bg-white border h-72
-                border-gray-200 rounded-lg shadow-lg">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-center text-gray-900">
-                    Your Historical Era Match</h5>
-                <p class="mb-3 font-normal text-gray-700 text-center pt-2 h-24">Time travel through
-                    the quiz
-                    and find out which period of history resonates with your spirit.</p>
-                <a href="#"
-                   class="flex justify-center px-8 items-center py-2 text-sm font-medium text-center
-                   text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                    Read more
-                </a>
-            </div>
+            <div id="default-carousel" class="relative w-full" data-carousel="slide">
+                <!-- Carousel wrapper -->
+                <div class="relative h-56 overflow-hidden md:h-96">
+                    <!-- Item 1 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="{{ asset('asset/img/banner.png') }}"
+                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                             alt="...">
+                    </div>
+                    <!-- Item 2 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="{{ asset('asset/img/banner.png') }}"
+                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                             alt="...">
+                    </div>
+                    <!-- Item 3 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="{{ asset('asset/img/banner.png') }}"
+                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                             alt="...">
+                    </div>
+                </div>
 
-            <div
-                class="flex flex-col gap-4 items-center max-w-sm p-6 bg-white border h-72
-                border-gray-200 rounded-lg shadow-lg">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-center text-gray-900">
-                    Are You a Safe Driver?</h5>
-                <p class="mb-3 font-normal text-gray-700 text-center pt-2 h-24"> Buckle up and test
-                    your
-                    road
-                    knowledge. Essential for everyone on the road wanting to ensure they're keeping
-                    safety a priority.</p>
-                <a href="#"
-                   class="flex justify-center px-8 items-center py-2 text-sm font-medium text-center
-                   text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                    Read more
-                </a>
+                <!-- Slider indicators -->
+                <div
+                    class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                    <button type="button" class="w-2 h-2 rounded-full" aria-current="true"
+                            aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                    <button type="button" class="w-2 h-2 rounded-full" aria-current="false"
+                            aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                    <button type="button" class="w-2 h-2 rounded-full" aria-current="false"
+                            aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                </div>
+
+                <!-- Slider controls -->
+                <button type="button"
+                        class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                        data-carousel-prev>
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 rounded-full bg-white/15 dark:bg-gray-800/30 group-hover:bg-white/50 group-focus:ring-2 group-focus:ring-white group-focus:outline-none">
+                        <svg class="w-2 h-2 text-white dark:text-gray-800 rtl:rotate-180"
+                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round"
+                                  stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button"
+                        class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                        data-carousel-next>
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 rounded-full bg-white/15 dark:bg-gray-800/30 group-hover:bg-white/50 group-focus:ring-2 group-focus:ring-white group-focus:outline-none">
+                        <svg class="w-2 h-2 text-white dark:text-gray-800 rtl:rotate-180"
+                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round"
+                                  stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
             </div>
 
         </section>
@@ -186,7 +204,7 @@
         </div>
     </section>
 
-    {{-- Modal --}}
+    {{-- Login Modal --}}
     <div id="authentication-modal" tabindex="-1" aria-hidden="true"
          class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-md max-h-full">
@@ -272,7 +290,6 @@
     window.addEventListener('beforeunload', function (e) {
         document.getElementById('loadingScreen').style.display = 'flex';
     });
-
     window.addEventListener('load', function (e) {
         document.getElementById('loadingOverlay').style.display = 'none';
     });
