d18f73270ab397302fc0d837ac6f6e7c5f462a0a
Khaled Hossain <khaledhossain778@gmail.com>
2024-04-03 10:19:17 +0600

    Update footer.blade.php

--- resources/views/components/common/footer.blade.php
+++ resources/views/components/common/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="#"
                    class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <img src="{{ asset('asset/img/logo/1.png') }}" class="h-10"
+                    <img src="{{ secure_asset('asset/img/logo/1.png') }}" class="h-10"
                          alt="Logo"/>
                     <span
                         class="self-center text-2xl font-semibold whitespace-nowrap">Quizlet</span>
